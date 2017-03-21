47d69452cfa6931226afc39c0d6f977ecdb3153b
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-21 16:53:22 +0100

    user input exercises + average does not mean any complicated thing

---
+++ week-02/day2/average-of-input.py
@@ -0,0 +1,13 @@
+print("Please, give me 5 intigers you like!")
+
+user_input1 = int(input())
+user_input2 = int(input())
+user_input3 = int(input())
+user_input4 = int(input())
+user_input5 = int(input())
+
+summ = user_input1 + user_input2 + user_input3 + user_input4 + user_input5
+
+average = summ / 5
+
+print("Summ: " + str(summ), "Average: " + str(average))
