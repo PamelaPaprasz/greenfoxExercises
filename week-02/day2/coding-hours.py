d644321c9a1f770c3d5b6adc3a927c2459364fa0
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-21 12:57:59 +0100

    doing math with variables

---
+++ week-02/day2/coding-hours.py
@@ -0,0 +1,10 @@
+dailyWorkingHours = 6
+daysInWeek = 5
+allWeek = 17
+
+
+print(6 * 5 * 17)
+
+avarageWorkHoursWeekly = 52
+
+print( dailyWorkingHours * daysInWeek / avarageWorkHoursWeekly * 100,"%")
