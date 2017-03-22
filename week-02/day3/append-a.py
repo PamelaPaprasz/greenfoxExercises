441b8c841c8a6d8b721b4475e6fee8676bb5860a
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-22 12:04:46 +0100

    add one letter to a string in two different ways, first is simple, second is using for loop and lists and .join method

---
+++ week-02/day3/append-a.py
@@ -0,0 +1,30 @@
+# - Create a variable named `am` and assign the value `kuty` to it
+# - Write a function called `appendA` that gets a string as an input
+#   and appends an 'a' character to its end
+# - Print the result of `appendA(am)`
+
+am = "kuty"
+
+def appendA(am):
+
+    am = am + "a"
+
+    print(am)
+
+appendA(am)
+
+
+a = "macsk"
+
+def real_append_fuction(a):
+
+    cuted_cat = []
+    for letter in a:
+        cuted_cat.append(letter)
+    print(cuted_cat)
+    cuted_cat.append("a")
+    print(cuted_cat)
+    macska = ''.join(cuted_cat)
+    print(macska)
+
+real_append_fuction(a)
