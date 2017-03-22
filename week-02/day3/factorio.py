48fec2c02fabfaf101c49d89ad2225f9f2c23d6e
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-22 23:26:48 +0100

    without 1 in the for loop this function wont work because anything multiplied by zero remains zero

--- week-02/day3/factorio.py
+++ week-02/day3/factorio.py
@@ -1,13 +1,12 @@
 # - Create a function called `factorio`
 #   that returns it's input's factorial
 
-number = 5
 
-def factorio(n):
+# without 1 in the for loop this function wont work because anything multiplied by zero remains zero
 
-    while number >= 1:
-        number = number * n
-        n = number - 1
-    return number
+def factorio(all_to_given_number):
+    for a in range(1, all_to_given_number):
+        all_to_given_number = a * all_to_given_number
+    print(all_to_given_number)
 
-    factorio(n)
+factorio(3)
