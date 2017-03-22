70cfe81edee1ff14ed450796abda0af59801649c
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-22 23:34:37 +0100

    you can save like 1 step by adding 1 in the for loop but the function will work anyway because no matter whether the counting starts from zero or not

--- week-02/day3/sum.py
+++ week-02/day3/sum.py
@@ -1,9 +1,28 @@
 # - Write a function called `sum` that sum all the numbers
 #   until the given parameter
-print("number comes here:")
 
+
+'''
+this code only sum the elements of list called "a" up
 a = [2, 6, 8, 9]
-
 a = sum(a)
-
 print(a)
+'''
+
+def ssum(all_to_given_number):
+    for a in range(all_to_given_number):
+        all_to_given_number = a + all_to_given_number
+    print(all_to_given_number)
+
+ssum(3)
+
+'''
+adding 1 before number in the for loop tells to the computer to start the counting from 1 instead of 0
+
+def summ(number):
+    for x in range(1,number):
+        number = x + number
+    print(number)
+
+summ(5)
+'''
