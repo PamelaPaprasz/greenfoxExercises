ebe60245d2d1ce4e4af2559973699568d2d6891d
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-23 15:08:20 +0100

    changed to elements of a list

---
+++ week-02/day4/candyshop.py
@@ -0,0 +1,28 @@
+'''
+
+import java.util.*;
+
+public class Candyshop{
+  public static void main(String... args){
+    ArrayList<Object> arrayList = new ArrayList<Object>();
+    arrayList.add("Cupcake");
+    arrayList.add(2);
+    arrayList.add("Brownie");
+    arrayList.add(false);
+
+    // Accidentally we added "2" and "false" to the list.
+    // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+    // No, don't just remove the lines
+
+    System.out.println(arrayList);
+  }
+}
+
+'''
+
+candyshop = ["Cupcake", 2, "Brownie", False]
+
+candyshop[1] = "Croissant"
+candyshop[3] = "Ice cream"
+
+print(candyshop)
