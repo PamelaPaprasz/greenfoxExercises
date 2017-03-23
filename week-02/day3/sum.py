9a89f68387f5e8bffe34f46d18261b5250e51017
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-23 21:44:52 +0100

    changed the argument of function from all_to_given_number to number because the first version did not make any sence at all since defining the  thing we will give our function to work with much better than define the process that will happen while using the function

--- week-02/day3/sum.py
+++ week-02/day3/sum.py
@@ -9,14 +9,36 @@ a = sum(a)
 print(a)
 '''
 
-def ssum(all_to_given_number):
-    for a in range(all_to_given_number):
-        all_to_given_number = a + all_to_given_number
-    print(all_to_given_number)
+def ssum(number):
+    for a in range(number):
+        number = a + number
+    print(number)
 
 ssum(3)
 
 '''
+
+function version where the number variable is out of the function => it will work always with 5 so we can not give any  other value to n in our function... this problem can be solved by changing n to number so our for loop will work with the number given after print sum because it will think that the two number variables are two differet thing
+
+number = 5
+
+def sum(n)
+    out = 0
+    for i in range(1, n + 1):
+        out += i
+    return out
+
+print(sum(number))
+
+number = 5
+out = 0
+
+for i in range(1, number + 1):
+    out +=i
+
+print(out)
+
+
 adding 1 before number in the for loop tells to the computer to start the counting from 1 instead of 0
 
 def summ(number):
