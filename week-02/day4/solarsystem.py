d6518e780c1513fbe981b141d1f352756ced191f
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-23 14:20:00 +0100

    used .insert() method insted of .append() so the item is inserted to the 5th place insted of the end of the row

---
+++ week-02/day4/solarsystem.py
@@ -0,0 +1,10 @@
+# Saturn is missing from the planetList
+# Insert it into the correct position
+
+planetList = ["Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"]
+
+planetList.insert(5, "Saturn")
+
+planetList = ' , '.join(planetList)
+
+print(planetList)
