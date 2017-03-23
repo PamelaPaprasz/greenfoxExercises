5dacf289228dd6ea6b68b720737fcaed5b9abf3a
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-23 11:49:47 +0100

    first converting string to list and then change the third word and then used .join method to print the result as a string instead of a list

---
+++ week-02/day4/simplereplace.py
@@ -0,0 +1,13 @@
+
+
+e = ['In', 'a', 'dishwasher', 'far', 'far', 'away']
+
+(e[2]) = 'galaxy'
+
+e = ' '.join(e)
+
+print(e)
+
+# I would like to replace "dishwasher" with "galaxy" in this example
+# Please fix it for me!
+# Expected ouput: In a galaxy far far away
