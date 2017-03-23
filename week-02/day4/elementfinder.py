bf3ff0af29063e4d323b27ce3877ef9270c4b48c
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-23 15:49:29 +0100

    finding an element in a list

---
+++ week-02/day4/elementfinder.py
@@ -0,0 +1,10 @@
+# Check if the list contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+numbers = [1,2,3,4,5,6,8];
+
+x = 7
+
+if x in numbers:
+    print("Hoorray")
+else:
+    print("Nooooo")
