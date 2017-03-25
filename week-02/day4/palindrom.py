636d4dfd4f16a8a9d0d30f8acffbe3b727781b2a
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-25 18:27:34 +0100

    it works but if I write a fir loop within the function for i in range(len(user_word), 0, -1): it repeates the result 4 times and it would be nice to know why

---
+++ week-02/day4/palindrom.py
@@ -0,0 +1,10 @@
+
+user_word = input('Enter the word here so I can palindromize it for you: ')
+
+def palindrom_creator(word):
+    user_word = word
+    user_word2 = user_word[::-1]
+    user_word += user_word2
+    return user_word
+
+print(palindrom_creator(user_word))
