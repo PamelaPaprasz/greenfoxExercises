a92950990bb2da1cd52cb95230c7129460ea8163
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-25 23:53:48 +0100

    since I cut out the first letter from the reversed version of the user input the function does not work if the user enter a letter only. To solve this issue I wrote an if statement to make sure that the input was more than a letter and there is an else statement too in case the input was onlz a letter

--- week-02/day4/palindrom.py
+++ week-02/day4/palindrom.py
@@ -1,11 +1,15 @@
 
-user_word = input('Enter the word here so I can palindromize it for you: ')
+user_word = input('Give me a word that I can palindromize for you: ')
 
 def palindrom_creator(word):
     user_word = word
-    user_word2 = user_word[::-1]
-    user_word2 = user_word2[1:]
-    user_word += user_word2
+    if len(user_word) > 1:
+        user_word2 = user_word[::-1]
+        user_word2 = user_word2[1:]
+        user_word += user_word2
+    else:
+        print('I can not reverse a letter, sorry, your choice was not long enough!')
+
     return user_word
 
 print(palindrom_creator(user_word))
