153224b08383c18a6a67572ec60f7ceeed646a58
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-25 23:05:14 +0100

    if statements are not enough we need a for loop eher to get the result

---
+++ week-02/day4/anagram.py
@@ -0,0 +1,31 @@
+'''Create a function named is anagram following your current language's style guide. It should take two strings and return a boolean value depending on whether its an anagram or not.
+'''
+'''
+x = input("first word: ")
+
+y = input("second word: ")
+
+def anagram(word1, word2):
+
+    for i in word1:
+        if i in word2:
+            return True
+        else:
+            return False
+
+print('These words are anagrams: ', anagram(x, y))
+
+'''
+x = input("first word: ")
+
+y = input("second word: ")
+
+def anagram(word1, word2):
+
+    for i in x:
+        if x in y:
+            return True
+        else:
+            return False
+
+print('These words are anagrams: ', anagram(x, y))
