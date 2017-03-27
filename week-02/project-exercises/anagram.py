9ea1dbd25067d330a30ab2021b9b4a91cc633a77
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-27 08:35:45 +0200

    the previous version was not giving the proper result because the  loop was stoping after it found the first matching pair of words.. in order to fix it I made a list out each of the user imput word and then put them in alphabetic order using .sort() methodde

--- week-02/project-exercises/anagram.py
+++ week-02/project-exercises/anagram.py
@@ -1,15 +1,43 @@
 '''Create a function named is anagram following your current language's style guide. It should take two strings and return a boolean value depending on whether its an anagram or not.
 '''
+
+'''
 x = input("first word: ")
 
 y = input("second word: ")
 
 def anagram(word1, word2):
 
-    for i in word1:
-        if i in word2:
-            return True
-        else:
-            return False
+    word1 = list(word1)
+    word2 = list(word2)
+
+    abc_order_word1 = word1.sort()
+    abc_order_word2 = word2.sort()
+
+    if abc_order_word1 == abc_order_word2:
+        return True
+    else:
+        return False
+
+print('These words are anagrams: ', anagram(x, y))
+
+'''
+
+x = input("first word: ")
+
+y = input("second word: ")
+
+def anagram(word1, word2):
+
+    word1 = list(word1)
+    word1.sort()
+
+    word2 = list(word2)
+    word2.sort()
+
+    if word1 == word2:
+        return True
+    else:
+        return False
 
 print('These words are anagrams: ', anagram(x, y))
