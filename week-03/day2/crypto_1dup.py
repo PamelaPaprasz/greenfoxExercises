d48c737bf2822b1ddad89b48ca3e44284d3bd49e
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-28 12:26:36 +0200

    use a for loop and then make an other variable use [::2] to read only each of the second elements from the file

--- week-03/day2/crypto_1dup.py
+++ week-03/day2/crypto_1dup.py
@@ -1,8 +1,10 @@
 # Create a method that decrypts the texts/duplicated_chars.txt
 
 def decrypt(file):
-    text1 = open(file, "r")
-    text2 = text1.read()
+    text1 = open(file).read()
+
+    for i in text1:
+        text2 = text1[::2]
     print(text2)
     text1.close()
 
