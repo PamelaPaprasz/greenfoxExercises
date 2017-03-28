5fafc7aba990ad73884ab63079949f1755548a79
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-28 13:19:01 +0200

    here I used .join method in order to print the result as a simple text would look like

--- week-03/day2/crypto_3revorder.py
+++ week-03/day2/crypto_3revorder.py
@@ -1,7 +1,11 @@
 # Create a method that decrypts texts/reversed_zen_order.txt
 
 def decrypt(file_name):
+    look_like_text = ''
     text = open(file_name).readlines()
-    return text[::-1]
+    text2 = text[::-1]
+    join_version = look_like_text.join(text2)
+    return join_version
+    close(text)
 
 print(decrypt('reversed-order.txt'))
