b9fd41e92b42d03de3b9918e4ce15ee8f7a4d774
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-28 13:17:30 +0200

    I forgot to save before I committed the previous comment

--- week-03/day2/crypto_3revorder.py
+++ week-03/day2/crypto_3revorder.py
@@ -1,18 +1,7 @@
 # Create a method that decrypts texts/reversed_zen_order.txt
-'''
+
 def decrypt(file_name):
     text = open(file_name).readlines()
     return text[::-1]
-
-print(decrypt('reversed-order.txt'))
-'''
-
-def decrypt(file_name):
-    look_like_text = ''
-    text = open(file_name).readlines()
-    text2 = text[::-1]
-    join_version = look_like_text.join(text2)
-    return join_version
-    close(text)
 
 print(decrypt('reversed-order.txt'))
