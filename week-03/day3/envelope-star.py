dbf3c4ed9239c2127b9c69d55faa3c905eda90be
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-30 01:48:15 +0200

    create a black star out of lines, finally I got how it works

--- week-03/day3/envelope-star.py
+++ week-03/day3/envelope-star.py
@@ -7,13 +7,16 @@ canvas.pack()
 
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/envelope-star/r2.png]
-'''
+
 for i in range(0, 151, 10):
 
     canvas.create_line(i + 150, 150, 150, i)
     canvas.create_line(150, i +150, i, 150)
     canvas.create_line(150, i + 150, 300 - i, 150)
     canvas.create_line(150, i, 150-i, 150)
+
+
+root.mainloop()
 
 '''
 canvas.create_line(150, 150, 300, 150)
@@ -24,6 +27,4 @@ canvas.create_line(150, 250, 200, 150)
 canvas.create_line(150, 280, 170, 150)
 canvas.create_line(150, 290, 160, 150)
 canvas.create_line(150, 300, 150, 150)
-
-
-root.mainloop()
+'''
