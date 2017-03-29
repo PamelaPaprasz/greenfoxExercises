e1f2a70c417d55eb632e9cf1ae81069485285e9e
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-29 14:33:15 +0200

    offset squares using for loop with 4 variables

--- week-03/day3/purple-steps.py
+++ week-03/day3/purple-steps.py
@@ -5,16 +5,19 @@ root = Tk()
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
-square = canvas.create_rectangle(30, 30, 20, 20, fill = "purple")
-square = canvas.create_rectangle(40, 40, 30, 30, fill = "purple")
-square = canvas.create_rectangle(50, 50, 40, 40, fill = "purple")
-square = canvas.create_rectangle(60, 60, 50, 50, fill = "purple")
-square = canvas.create_rectangle(70, 70, 60, 60, fill = "purple")
-square = canvas.create_rectangle(80, 80, 70, 70, fill = "purple")
-square = canvas.create_rectangle(90, 90, 80, 80, fill = "purple")
-square = canvas.create_rectangle(100, 100, 90, 90, fill = "purple")
-square = canvas.create_rectangle(110, 110, 100, 100, fill = "purple")
-square = canvas.create_rectangle(120, 120, 110, 110, fill = "purple")
+xtl = 10
+ytl = 10
+xbr = 20
+ybr= 20
+
+for i in range(19):
+
+    square = canvas.create_rectangle(xtl, ytl, xbr, ybr, fill = "purple")
+
+    xtl += 10
+    ytl += 10
+    xbr += 10
+    ybr += 10
 
 
 root.mainloop()
