161dc6a71fa6a039c4752031c965db43c7a2977c
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-31 23:25:10 +0200

    add colors and speed it up a bit

--- week-03/project-exercises/hexagon2.py
+++ week-03/project-exercises/hexagon2.py
@@ -7,13 +7,13 @@ canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
 def first_square(x, y, size):
-    canvas.create_polygon(x, y, size*1/4 + x, y - size*1/2, size*3/4 + x, y - size*1/2, size + x, y, size*3/4 + x, size*1/2 + y, size*1/4 + x, size*1/2 + y, fill = "", outline = 'black')
+    canvas.create_polygon(x, y, size*1/4 + x, y - size*1/2, size*3/4 + x, y - size*1/2, size + x, y, size*3/4 + x, size*1/2 + y, size*1/4 + x, size*1/2 + y, fill = "red", outline = 'blue')
 
 #print(first_square(80, 300, 500))
 
 def hexagon_in_hexagon(x, y, size):
     first_square(x, y, size)
-    time.sleep(0.1)
+    time.sleep(0.01)
     canvas.update()
 
     if size > 10:
