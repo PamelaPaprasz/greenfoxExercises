676ef5a754f490fea62ed17d05c7935e471be3ff
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-31 21:42:59 +0200

    create a superhexagon again but there is something strange is going on with the size of its side and would be nice to have it fixed

---
+++ week-03/project-exercises/hexagon2.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='600', height='600')
+canvas.pack()
+
+def first_square(x, y, size):
+    canvas.create_polygon(x, y, size*1/4 + x, y - size*1/2, size*3/4 + x, y - size*1/2, size + x, y, size*3/4 + x, size*1/2 + y, size*1/4 + x, size*1/2 + y, fill = "", outline = 'black')
+
+#print(first_square(80, 300, 500))
+
+def hexagon_in_hexagon(x, y, size):
+    first_square(x, y, size)
+
+    if size > 10:
+        hexagon_in_hexagon(x + size*1/6, y - size*1/3, size*1/3)
+
+        hexagon_in_hexagon(x, y, size*1/3)
+
+        hexagon_in_hexagon(x + size*1/6, y + size*1/3, size*1/3)
+
+        hexagon_in_hexagon(x + size*1/2, y + size*1/3, size*1/3)
+
+        hexagon_in_hexagon(x + size*1/2, y - size*1/3, size*1/3)
+
+        hexagon_in_hexagon(x + size*1/1.5, y + size*1/120, size*1/3)
+
+hexagon_in_hexagon(80, 300, 400)
+
+root.mainloop()
