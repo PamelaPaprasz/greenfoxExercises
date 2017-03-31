4dd71771fff8197fea9b2b9d06ad72ba06304104
Pamela Paprasz <papraszpamela@gmail.com>
2017-03-31 15:04:10 +0200

    delet comments

--- week-03/demo3/something.py
+++ week-03/demo3/something.py
@@ -6,10 +6,6 @@ canvas = Canvas(root, width='800', height='800')
 canvas.pack()
 
 canvas.create_oval(200, 200, 100, 100, fill = 'black', outline = 'black')
-#canvas.create_oval(200, 200, 150, 150, fill = 'black', outline = 'black')
-#canvas.create_oval(300, 300, 170, 170, fill = 'black', outline = 'black')
-#canvas.create_oval(500, 500, 0, 0, fill = 'black', outline = 'black')
-#canvas.create_oval(100, 100, 70, 70, fill = 'black', outline = 'black')
 canvas.create_line(420, 400, 600, 600, fill = 'blue', width = 5)
 canvas.create_line(420, 400, 500, 600, fill = 'indigo', width = 5)
 canvas.create_line(420, 400, 400, 600, fill = 'violet', width = 5)
@@ -26,7 +22,5 @@ canvas.create_line(400, 50, 300, 150, fill = 'black', width = 10)
 canvas.create_line(150, 300, 200, 600, fill = 'black', width = 10)
 canvas.create_line(300, 150, 600, 200, fill = 'black', width = 10)
 canvas.create_line(600, 200, 200, 600, fill = 'black', width = 10)
-
-
 
 root.mainloop()
