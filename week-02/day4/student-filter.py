fa25af21a6f64e0cfa62cf4d6311be1d141b740b
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-01 13:41:03 +0200

    finish the exercise

--- week-02/day4/student-filter.py
+++ week-02/day4/student-filter.py
@@ -19,5 +19,15 @@ more_than(students)
 # create a function that takes a list of students and prints:
 # - Who has got more candies than 4 candies
 
+def avarage_candy(num_candy):
+
+    all_candies = 0
+    for i in num_candy:
+        all_candies += i['candies']
+    avarage = all_candies / len(students) #len is a must in order to get to know how many elements our list has
+    return avarage
+
+print(avarage_candy(students))
+
 # create a function that takes a list of students and prints:
 #  - how many candies they have on average
