8146199ad99a5a46acb4b2012220e20d50ec160c
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-01 23:04:55 +0200

    do the trees

---
+++ week-04/homework.py
@@ -0,0 +1,9 @@
+tree1 = {'name': 'lost', 'leaf_color': 'green', 'age': 32, 'sex': 'female'}
+
+tree2 = {'name': 'wonderboom', 'leaf_color': 'green', 'age': 3, 'sex': 'male'}
+
+tree3 = {'name': 'boabab', 'leaf_color': 'green', 'age': 1060, 'sex': 'male'}
+
+tree4 = {'name': 'balete', 'leaf_color': 'green', 'age': 1300, 'sex': 'female'}
+
+tree5 = {'name': 'life', 'leaf_color': 'green', 'age': 400, 'sex': 'female'}
