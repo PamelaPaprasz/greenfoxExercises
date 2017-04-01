6cf57bb4b4bd2493be7c62b2476e43bb62f51180
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-01 12:08:59 +0200

    fix the task using 3 for loop

--- week-02/day4/hewillnever.py
+++ week-02/day4/hewillnever.py
@@ -2,11 +2,8 @@
 # Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
 # Assemble the fragments into the out variable
 
-out = {7: "run around and desert you", 50: "tell a lie and hurt you", 49: "make you cry,", 2: "let you down", 12: "give you up,", 1: "Never gonna", 11: "\n", 3: "say goodbye"}
+out = ""
 
-print(out[11], out[1], out[12], out[1], out[2], out[11], out[1], out[7], out[11], out[49], out[1], out[3], out[11], out[1], out[50], out[11])
-
-out = (hashmap[5] = notSoCrypticMessage[0]);
 notSoCrypticMessage = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11]
 
 hashmap = [
@@ -19,5 +16,11 @@ hashmap = [
     {11: "\n"},
     {3: "say goodbye"}
 ]
+
+for i in notSoCrypticMessage: #here you pick an element up from the notSoCrypticMessage
+    for a in hashmap: #here you have this element from the notSoCrypticMessage and go through the elements of the hashmap but here we know only the list indexes of the hashmap
+        for k, v in a.items(): # here you go inside to each of the dictionaries and look through the elements using a.items()
+            if i == k: # and if you find that the element from notSoCrypticMessage is equal to the key value of one of the dictionaries
+                out += v #you give the value of the matching key to the out variable
 
 print(out)
