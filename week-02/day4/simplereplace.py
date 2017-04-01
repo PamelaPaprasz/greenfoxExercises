79bdc9128878996c1c8dcb39e3196bb5117b66e0
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-01 23:30:00 +0200

    do the exercise again using .split method

--- week-02/day4/simplereplace.py
+++ week-02/day4/simplereplace.py
@@ -1,13 +1,11 @@
 
-
-e = ['In', 'a', 'dishwasher', 'far', 'far', 'away']
-
-(e[2]) = 'galaxy'
-
-e = ' '.join(e)
-
-print(e)
-
 # I would like to replace "dishwasher" with "galaxy" in this example
 # Please fix it for me!
 # Expected ouput: In a galaxy far far away
+
+e = 'In a dishwasher far far away'
+
+words = e.split()
+words[2] = 'galaxy'
+e = ' '.join(words)
+print(e)
