1d4e4907bfb3933c96b434e03a147728dcaaf7ac
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-03 11:34:17 +0200

    recreate the exercise without using init methods

--- week-04/post-it.py
+++ week-04/post-it.py
@@ -1,11 +1,22 @@
-class PostIt():
-    def __init__(self, backgroundColor, text, textColor):
-        self.backgroundColor = backgroundColor
-        self.text = text
-        self.textColor = textColor
+class PostIt:
+    background_color = ''
+    text = ''
+    text_color = ''
 
-post_it_1 = PostIt("orange", "Idea1", "blue")
-post_it_2 = PostIt("pink", "Awsome", "black")
-post_it_3 = PostIt("yellow", "Superb!", "green")
+post_it_1 = PostIt()
+post_it_2 = PostIt()
+post_it_3 = PostIt()
 
-print (post_it_1.backgroundColor, post_it_1.text, post_it_1.textColor)
+post_it_1.background_color = 'orange'
+post_it_1.text = 'Idea1'
+post_it_1.text_color = 'blue'
+
+post_it_2.background_color = 'pink'
+post_it_2.text = 'Awesome'
+post_it_2.text_color = 'black'
+
+post_it_3.background_color = 'yellow'
+post_it_3.text = 'Superb!'
+post_it_3.text_color = 'green'
+
+print ('Backgroundcolor of this post it is ' + post_it_1.background_color + ', the text on it is ' + post_it_1.text + ' and the color of the text is ' +  post_it_1.text_color)
