01ccd24f5575e4f70208d377551f0222f0bb780e
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-05 14:44:28 +0200

    write a test for anagram

--- week-04/day3/test.py
+++ week-04/day3/test.py
@@ -5,7 +5,6 @@ from work import Apple
 class testApple(unittest.TestCase):
     def test_get_apple(self):
         apple = Apple()
-        apple.get_apple()
         self.assertEqual(apple.get_apple(), apple.fruit_type)
 
     def test_sum_list(self):
@@ -16,13 +15,13 @@ class testApple(unittest.TestCase):
         apple = Apple()
         self.assertEqual(apple.sum([]), 0)
 
-    def test_sum_empty_list(self):
-        apple = Apple()
-        self.assertEqual(apple.sum([]), 0)
-
     def test_sum_one_element_list(self):
         apple = Apple()
         self.assertEqual(apple.sum([4]), 4)
+
+    def test_anagram(self):
+        apple = Apple()
+        self.assertEqual(apple.anagram('dad', 'dad'), True)
 
 if __name__ == '__main__':
     unittest.main()
