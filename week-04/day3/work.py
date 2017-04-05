2da3533c5fa48588d0d729b373e73514a50fd2d7
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-05 14:26:56 +0200

    create an anamgram checker function

--- week-04/day3/work.py
+++ week-04/day3/work.py
@@ -1,33 +1,3 @@
-'''
-Create a class, with one method (eg. get_apple()) that returns a string (eg. "apple")
-Create a test for that:
-Create a test class
-Create a test method
-Instantiate an Object from your class in the method
-Use the assertEquals()
-The expected parameter should be the same string (eg. "apple")
-The actual parameter should be the return value of the method (eg. myobject.get_apple())
-Run the test
-Change the expected value to make the test failing
-Run the test
-Fix the returned value to make the test succeeding again
-
-Create a sum method in your class which has a list of integers as parameter
-It should return the sum of the elements in the list
-Follow these steps:
-Add a new test case
-Instantiate your class
-create a list of integers
-use the assertEquals to test the result of the created sum method
-Run it
-Create different tests where you
-test your method with an empyt list
-with a list with one element in it
-with multiple elements in it
-with a null
-Run them
-Fix your code if needed
-'''
 
 class Apple():
 
@@ -43,9 +13,24 @@ class Apple():
             count += i
         return count
 
+    def anagram(self, str1, str2):
+        self.str1 = str1
+        self.str2 = str2
+
+        list_str1 = list(str1)
+        list_str2 = list(str2)
+        list_str1.sort()
+        list_str2.sort()
+
+        if list_str1 == list_str2:
+            return True
+        else:
+            return False
+
 
 
 apple = Apple()
 apple.get_apple()
 print(apple.fruit_type)
 print(apple.sum([2, 5, 6, 7]))
+print(apple.anagram('dad', 'bad'))
