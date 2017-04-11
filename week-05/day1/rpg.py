4b6830b68eb640ee50624a6282cef5e4deaed4d0
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-11 17:47:56 +0200

    feat(on_key_press, draw): keys are working and start_hero image is always deleted after moving to somewhere else

--- week-05/day1/rpg.py
+++ week-05/day1/rpg.py
@@ -39,9 +39,11 @@ class Hero():
         self.image_hero_up = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-up.png')
         self.hero_x = 36
         self.hero_y = 36
+        self.start_hero = 0
 
-    def draw(self):
-        self.start_hero = canvas.create_image(self.hero_x, self.hero_y, image = self.image_hero_down)
+    def draw(self, canvas, hero_image):
+        canvas.delete(self.start_hero)
+        self.start_hero = canvas.create_image(self.hero_x, self.hero_y, image = hero_image)
 
 
 class GameLogic():
@@ -52,17 +54,17 @@ class GameLogic():
         self.e = e 
         
         if self.e.keycode == 37:
-            hero.hero_x = self.hero_x - 72
-            hero.draw()
+            hero.hero_x = hero.hero_x - 72
+            hero.draw(canvas, hero.image_hero_left)
         elif self.e.keycode == 38:
-            hero.hero_y = self.hero_y -72
-            hero.draw()
+            hero.hero_y = hero.hero_y -72
+            hero.draw(canvas, hero.image_hero_up)
         elif self.e.keycode == 39:
-            hero.hero_x = self.hero_x + 72
-            hero.draw()
+            hero.hero_x = hero.hero_x + 72
+            hero.draw(canvas, hero.image_hero_right)
         elif self.e.keycode == 40:
-            hero.hero_y = self.hero_y + 72
-            hero.draw()
+            hero.hero_y = hero.hero_y + 72
+            hero.draw(canvas, hero.image_hero_down)
                 
 
 
@@ -70,7 +72,7 @@ floor = Tile()
 hero = Hero()
 logic = GameLogic()
 floor.draw()
-hero.draw()
+hero.draw(canvas, hero.image_hero_down)
 
 
 canvas.pack()
