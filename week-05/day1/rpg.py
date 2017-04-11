af01905c17acaf85ab4818b078c3770557152f23
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-11 17:02:52 +0200

    feat(hero, gamelogic, baddie):new classes are added

--- week-05/day1/rpg.py
+++ week-05/day1/rpg.py
@@ -9,13 +9,6 @@ class Tile():
     def __init__(self):
         self.image_floor = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\floor.png')
         self.image_wall = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\wall.png')
-        self.image_boss = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\boss.png')
-        self.image_skeleton = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\skeleton.png')
-        self.image_hero_down = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-down.png')
-        self.image_hero_left = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-left.png')
-        self.image_hero_right = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-right.png')
-        self.image_hero_up = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-up.png')
-        #self.root.bind("<Key>", self.key)
         
     def draw(self):
         self.first_square = canvas.create_image(36, 36, image = self.image_floor)
@@ -31,11 +24,42 @@ class Tile():
         for index_main_list in range(len(self.wall_matrix)):
             for index_sub_list in self.wall_matrix[index_main_list]:
                 canvas.create_image(36 + 72 * index_sub_list, 36 + 72 * index_main_list, image = self.image_wall)
-                
+                    
+
+class Baddie():
+    def __init__(self):
+        self.image_boss = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\boss.png')
+        self.image_skeleton = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\skeleton.png')
+
+
+class Hero():
+    def __init__(self):
+        self.image_hero_down = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-down.png')
+        self.image_hero_left = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-left.png')
+        self.image_hero_right = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-right.png')
+        self.image_hero_up = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-up.png')
+
+    def draw(self):
         self.start_hero = canvas.create_image(36, 36, image = self.image_hero_down)
+
+
+class GameLogic(Tile):
+    
+    def on_key_press(e):
+        if e.keycode == 37:
+            self.image_hero_left = self.image_hero_left - 72
+        elif e.keycode == 38:
+            self.image_hero_up = self.image_hero_up -72
+        elif e.keycode == 39:
+            self.image_hero_right = self.image_hero_right + 72
+        elif e.keycode == 40:
+            self.image_hero_down = self.image_hero_down + 72
                 
+    canvas.bind("<KeyPress>", on_key_press)
 
 
 floor = Tile()
+hero = Hero()
 floor.draw()
+hero.draw()
 root.mainloop()
