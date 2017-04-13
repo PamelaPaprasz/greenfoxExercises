3e8f957c8e054101e3c90c844689be7c857a8858
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-13 15:01:10 +0200

    refact(Baddie, GameLogic):make the names of the variables more simple

--- week-05/day1/rpg.py
+++ week-05/day1/rpg.py
@@ -7,8 +7,8 @@ canvas = Canvas(root, width='720', height='720')
 
 class Tile():
     def __init__(self):
-        self.image_floor = PhotoImage(file= r'd:\greenfox\PamelaPaprasz\week-05\day1\floor.png')
-        self.image_wall = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\wall.png')
+        self.image_floor = PhotoImage(file= 'floor.png')
+        self.image_wall = PhotoImage(file = 'wall.png')
         
     def draw_map(self):
         
@@ -36,25 +36,27 @@ class Tile():
 
 class Baddie():
     def __init__(self):
-        self.image_boss = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\boss.png')
-        self.image_skeleton = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\skeleton.png')
-        self.skeleton_x = random.randint(0, 9)
-        self.skeleton_y = random.randint(0, 9)
-        self.start_skeleton = 0
+        self.image_boss = PhotoImage(file = 'boss.png')
+        self.image_skeleton = PhotoImage(file = 'skeleton.png')
+        self.x = random.randint(0, 9)
+        self.y = random.randint(0, 9)
+        self.id = 0
+                
+    def draw_skeleton(self, canvas):
+        canvas.delete(self.id)
+        self.id = canvas.create_image(self.x * 72, self.y * 72, anchor = NW, image = self.image_skeleton)
         
+    def draw_boss(self, canvas):
+        canvas.delete(self.id)
+        self.id = canvas.create_image(self.x * 72, self.y * 72, anchor = NW, image = self.image_boss)
         
-    def draw_skeleton(self, canvas, image_skeleton):
-        canvas.delete(self.start_skeleton)
-        self.start_skeleton = canvas.create_image(self.skeleton_x * 72, self.skeleton_y * 72, anchor = NW, image = image_skeleton)
-        
-
 
 class Hero():
     def __init__(self):
-        self.image_hero_down = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-down.png')
-        self.image_hero_left = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-left.png')
-        self.image_hero_right = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-right.png')
-        self.image_hero_up = PhotoImage(file = r'd:\greenfox\PamelaPaprasz\week-05\day1\hero-up.png')
+        self.image_hero_down = PhotoImage(file = 'hero-down.png')
+        self.image_hero_left = PhotoImage(file = 'hero-left.png')
+        self.image_hero_right = PhotoImage(file = 'hero-right.png')
+        self.image_hero_up = PhotoImage(file = 'hero-up.png')
         self.hero_x = random.randint(0, 9)
         self.hero_y = random.randint(0, 9)
         self.start_hero = 0
@@ -62,7 +64,6 @@ class Hero():
         self.hero_hp = 20 + 3 * random.randint(1, 6)
         self.hero_dp = 2 * random.randint(1, 6)
         self.hero_sp = 5 + random.randint(1, 6)
-
 
     def draw_hero(self, canvas, hero_image):
         canvas.delete(self.start_hero)
@@ -81,6 +82,8 @@ class GameLogic():
         self.skeleton3 = Baddie()
         self.skeletons = [self.skeleton1, self.skeleton2, self.skeleton3]
         self.is_it_free_for_skeleton()
+        self.boss = Baddie()
+        self.is_it_free_for_boss()
     
     def is_it_free_for_hero(self, x, y):
         if 0 <= x <= 9 and 0 <= y <= 9:
@@ -91,10 +94,16 @@ class GameLogic():
                             
     def is_it_free_for_skeleton(self):
         for skeleton in self.skeletons:
-            while self.floor.map_matrix[skeleton.skeleton_y][skeleton.skeleton_x] != 0:
-                skeleton.skeleton_x = random.randint(0, 9)
-                skeleton.skeleton_y = random.randint(0, 9)
-            skeleton.draw_skeleton(canvas, skeleton.image_skeleton)
+            while self.floor.map_matrix[skeleton.y][skeleton.x] != 0:
+                skeleton.x = random.randint(0, 9)
+                skeleton.y = random.randint(0, 9)
+            skeleton.draw_skeleton(canvas)
+            
+    def is_it_free_for_boss(self):
+        while self.floor.map_matrix[self.boss.y][self.boss.x] != 0:
+            self.boss.x = random.randint(0, 9)
+            self.boss.y = random.randint(0, 9)
+        self.boss.draw_boss(canvas)
                             
     def on_key_press(self, e):
         self.e = e 
