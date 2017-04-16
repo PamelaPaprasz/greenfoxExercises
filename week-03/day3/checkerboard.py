4d9e4321930bf00c7563529d0fdb32d0ff2f88ff
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-16 20:58:15 +0200

    checkerboard is done

--- week-03/day3/checkerboard.py
+++ week-03/day3/checkerboard.py
@@ -1,13 +1,18 @@
+import time
 from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+canvas = Canvas(root, width='640', height='640')
 canvas.pack()
 
-
-
-
-# fill the canvas with a checkerboard pattern.
+for i in range(8):
+    for e in range(8):
+        time.sleep(0.1)
+        if (e+i) %2 == 0:
+            canvas.create_rectangle(0 + 80*e, 0 + 80*i, 160 + 160*e, 160 + 160*i, fill = 'white')
+        else:
+            canvas.create_rectangle(0 + 80*e, 0 + 80*i, 160 + 160*e, 160 + 160*i, fill = 'black')
+        canvas.update()
 
 root.mainloop()
