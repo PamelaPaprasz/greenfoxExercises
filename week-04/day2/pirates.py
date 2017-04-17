a5b1220833012d0712c122d3b947d8987fbb5bab
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-17 14:01:23 +0200

    feat(Ship class): fill the ship and get status methods are working, plus we can create captain too

--- week-04/day2/pirates.py
+++ week-04/day2/pirates.py
@@ -1,57 +1,54 @@
 class Pirate():
-    def __init__(self):
+    def __init__(self, captain = False):
         self.rum_counter = 0
-        self.dead = 0
+        self.dead = False
+        self.captain = captain
     
     def drink_some_rum(self):
         self.rum_counter += 1
         
     def hows_it_going_mate(self):
         
-        if self.rum_counter <= 4 and self.dead == 0: 
+        if self.rum_counter <= 4 and self.dead == False: 
             print('Pour me anudder!')
-        elif self.dead == 1:
+        elif self.dead == True:
             print('He is dead.')
         else:
             print('Arghh, I\'ma Pirate. How d\'ya d\'ink its goin?')
         
     def die(self):
-        self.dead = 1
+        self.dead = True
     
     def brawl(self, another_pirate):
         pass
         
     def get_status(self):
-        print('Drunk: {}, dead: {}.'.format(self.rum_counter,  self.dead))
+        print('Is he the captain? {} How many rum did hi drink? {} Is he dead already? {}.'.format(self.captain, self.rum_counter,  self.dead))
         
         
 class Ship(Pirate):
-    def __init__(self, pirate = 0, captain = 0):
+    def __init__(self):
         self.crew = []
-        self.pirate = pirate
-        self.captain = captain
     
-    def fill_ship(self):
-        self.pirate += 1
-        if self.captain == 0:
-            self.captain += 1
-        self.crew.append(self.pirate)
-        self.crew.append(self.captain)
+    def fill_ship(self, pirate):
+        self.crew.append(pirate)
+
+    def get_ship_status(self):
+        print('The ship has {} pirates on the board'.format(len(self.crew)))
+        for pirate in self.crew:
+            pirate.get_status()
         
-
-
+                
 pirate1 = Pirate()
 pirate2 = Pirate()
-captain = Pirate()
+captain = Pirate(captain = True)
 ship = Ship()
-for i in range(10):
-    ship.fill_ship()
-pirate1.get_status()
-#ship.get_status()
+ship.fill_ship(pirate1)
+ship.fill_ship(pirate2)
+ship.fill_ship(captain)
 pirate1.drink_some_rum()
 pirate1.hows_it_going_mate()
 pirate2.die()
 pirate2.hows_it_going_mate()
-
-        
-        
+ship.get_ship_status()
+    
