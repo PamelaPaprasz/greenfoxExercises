a29906e71ad39f6c14d94e01f4dfab50c6bb34a0
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-02 19:39:59 +0200

    initiate a new variable called s in order to manipulate the number of stars

--- week-07/day2/draw-diamond.js
+++ week-07/day2/draw-diamond.js
@@ -18,15 +18,16 @@
 // The diamond should have as many lines as lineCount is
 
 var star = '*';
+var s = 0;
 var paddingSpace = ' ';
 var lineCount = 7;
 
 for (var i = 0; i <= lineCount; i++){
     if (i < (lineCount/2)){
-        console.log(paddingSpace.repeat(lineCount-(i)) + star + paddingSpace.repeat(lineCount-(i)));
-        star += '**';
+        console.log(paddingSpace.repeat(lineCount-(i)) + star.repeat(s + 1) + paddingSpace.repeat(lineCount-(i)));
+        s += 2;
     } else{
-        star -= '**';
-        console.log(paddingSpace.repeat(i) + star + paddingSpace.repeat(i));
+        console.log(paddingSpace.repeat(i) + star.repeat(s-1) + paddingSpace.repeat(i));
+        s -= 2;
     };
 };
