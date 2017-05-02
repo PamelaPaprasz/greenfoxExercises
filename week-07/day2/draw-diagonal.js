eeb6683800370ea7d1bb91cd6d41722fb1fe6d95
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-03 01:29:55 +0200

    finally I came to realize that this image represents a letter 'n'

--- week-07/day2/draw-diagonal.js
+++ week-07/day2/draw-diagonal.js
@@ -15,22 +15,16 @@
 // The square should have as many lines as lineCount is
 
 
-var lineCount = 6;
+var lineCount = 16;
 var symbol = '%';
 var space = ' ';
 
 for (var i = 0; i < lineCount; i++){
-    if (i === 0 || i === lineCount-1){
+    if (i === 0 || i === lineCount -1){
         console.log(symbol.repeat(lineCount-1));
-    } else if (i === 1){
-        console.log(symbol.repeat((lineCount/2)-1) + space.repeat((lineCount/2)-1) + symbol.repeat(1));    
-    } else if (i === 2){
-            var repeatPattern = symbol.repeat(1) + space.repeat(1);
-            console.log(repeatPattern.repeat(lineCount/2));
-    } else if (i === 3){
-        console.log(symbol.repeat(1) + space.repeat((lineCount/2)-1) + symbol.repeat((lineCount/2)-1)); 
-    } else if (i === 4){
-        console.log(symbol.repeat(1) + space.repeat(lineCount/2) + symbol.repeat(1));
+    } else if (i === lineCount - 2){
+        console.log(symbol.repeat(1) + space.repeat(i - 1) + symbol.repeat(1));    
+    } else{
+        console.log(symbol.repeat(1) + space.repeat(i - 1) + symbol.repeat(1) + space.repeat(lineCount - i - 3) + symbol.repeat(1)); 
     };
 };
-
