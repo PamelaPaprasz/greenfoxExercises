dd0bbc9f6f07ea98c76d747362538868b8031417
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-03 15:37:40 +0200

    use replace method on a string

---
+++ week-07/day3/simplereplace.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+var example = "In a dishwasher far far away";
+
+example = example.replace(/dishwasher/i, 'galaxy');
+
+console.log(example)
+
