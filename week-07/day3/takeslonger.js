6f9722be239b2a12a97100bcb2bb42503c3833c2
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-03 16:21:14 +0200

    practicing splice method

---
+++ week-07/day3/takeslonger.js
@@ -0,0 +1,12 @@
+'use strict';
+// When saving this quote a disk error has occured. Please fix it.
+// Add "always takes longer than" to between the words "It" and "you"
+
+var quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law."
+
+quote = quote.split('');
+quote.splice(20, 0, ' always takes longer than');
+quote = quote.join('');
+
+console.log(quote);
+
