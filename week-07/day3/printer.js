c42e1a772799018a9af6c039827d64e2a466a3c9
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-03 12:02:26 +0200

    function can have any number of arguments and use toString method to convert array to string

---
+++ week-07/day3/printer.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Create a function called `printer`
+//   which logs to the console the input parameters
+//   (can have multiple number of arguments)
+
+function printer(){
+    arguments.toString();
+    for (var i = 0; i < arguments.length; i++){
+        console.log(arguments[i]);    
+    };
+};
+
+printer('valami', 'barmi', 'amit akarsz');
