972d917d7f8c83aebfe57a5d261a7e29f1001e9a
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-04 18:20:15 +0200

    no need any for loop

---
+++ week-07/day4/classlist-1.js
@@ -0,0 +1,19 @@
+var p = document.querySelectorAll('p');
+
+    
+if (p[2].classList.contains('cat')){
+    alert('YEAH CAT!');    
+};
+
+if (p[3].classList.contains('dolphin')){
+    p[0].textContent = changeFru;        
+};
+
+if (p[0].classList.contains('apple')){
+    p[2] = 'dog';
+};
+
+
+
+
+
