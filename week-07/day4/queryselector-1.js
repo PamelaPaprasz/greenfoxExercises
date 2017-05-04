5fe34378003cd7ae99aefd661cf0617c12eb7212
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-04 12:19:24 +0200

    practice query and alert

---
+++ week-07/day4/queryselector-1.js
@@ -0,0 +1,23 @@
+var king = document.getEelementById('b325');
+console.log(king);
+
+var conceited = document.querySelector('.b326').innerHTML;
+console.log(conceited);
+alert(conceited);
+
+var businessLamp = document.getElementsByClassName('asteroid big', 'asteroid b329 big');
+console.log(businessLamp);
+
+var conceitedKing = document.querySelectorAll('section > div');
+console.log(conceitedKing);
+var first_01 = conceitedKing[0].innerHTML;
+var second_01 = conceitedKing[1].innerHTML;
+alert(first_01);
+alert(second_01);
+
+var noBusiness  = document.querySelectorAll('div');
+console.log(noBusiness);
+
+var allBizniss = document.querySelector('p').innerHTML;
+console.log(allBizniss);
+alert(allBizniss);
