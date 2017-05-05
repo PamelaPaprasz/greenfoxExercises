d9cfee01538e1e7963e6e1a8ed1c48cbea4a785b
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-05 14:40:27 +0200

    array of images

---
+++ week-07/day5/image-collection.js
@@ -0,0 +1,52 @@
+var images = [
+    {
+        img: 'kezd.jpg',
+        title: 'massza',
+        discr: 'muszi',
+    },
+    {
+        img: 'actress.jpg',
+        title: 'sir',
+        discr: 'temeto',
+    },
+    {
+        img: 'cemetry.jpg',
+        title: 'sicilia',
+        discr: 'seta',
+    },
+    {
+        img: 'ejha.jpg',
+        title: 'ejhakutya',
+        discr: 'balaton',
+    },
+    {
+        img: 'kod.jpg',
+        title: 'legmagasabb pont',
+        discr: 'mo',
+    },
+    {
+        img: 'mama.jpg',
+        title: 'nagyi',
+        discr: 'kedvenc',
+    },
+    {
+        img: 'market.jpg',
+        title: 'nyami',
+        discr: 'zold',
+    },
+    {
+        img: 'office.jpg',
+        title: 'munka',
+        discr: 'uk',
+    },
+    {
+        img: 'rails.jpg',
+        title: 'vonat',
+        discr: 'tenger',
+    },
+    {
+        img: 'slide.jpg',
+        title: 'haha',
+        discr: 'tip',
+    }
+]
