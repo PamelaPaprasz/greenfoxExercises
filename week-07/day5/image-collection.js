8dde1cc25a52aee1bfdadd966f0b7f5b781a2a45
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-06 23:02:10 +0200

    id variables are added to objects

--- week-07/day5/image-collection.js
+++ week-07/day5/image-collection.js
@@ -3,51 +3,61 @@ var openingTitle = '!!!!!!!!! u wont see this cat ever again !!!!!!!!!!';
 var images = [
     {
         img: 'kezd.jpg',
+        id: 'one',
         title: 'massza',
         discr: 'muszi',
     },
     {
         img: 'actress.jpg',
+        id: 'two',
         title: 'sir',
         discr: 'temeto',
     },
     {
         img: 'cemetry.jpg',
+        id: 'three',
         title: 'sicilia',
         discr: 'seta',
     },
     {
         img: 'ejha.jpg',
+        id: 'four',
         title: 'ejhakutya',
         discr: 'balaton',
     },
     {
         img: 'kod.jpg',
+        id: 'five',
         title: 'legmagasabb pont',
         discr: 'mo',
     },
     {
         img: 'mama.jpg',
+        id: 'six',
         title: 'nagyi',
         discr: 'kedvenc',
     },
     {
         img: 'market.jpg',
+        id: 'seven',
         title: 'nyami',
         discr: 'zold',
     },
     {
         img: 'office.jpg',
+        id: 'eight',
         title: 'munka',
         discr: 'uk',
     },
     {
         img: 'rails.jpg',
+        id: 'nine',
         title: 'vonat',
         discr: 'tenger',
     },
     {
         img: 'slide.jpg',
+        id: 'ten',
         title: 'haha',
         discr: 'tip',
     }
