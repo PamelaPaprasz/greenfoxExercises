1b972bec2aae3ff37e955fc602ee254d0489a59f
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-06 03:51:46 +0200

    still cant click on thumbnails but maybe figured out some solution by adding numbers to each thumbnail innerHTML

--- week-07/day5/image-slider.js
+++ week-07/day5/image-slider.js
@@ -5,23 +5,77 @@ var rightBut = document.querySelector('.right');
 var currentText = document.querySelector('p');
 // var currentImg = document.querySelector('img');
 var i = -1;
+var t = 0;
 callImage('cic.jpg');
 callText();
 
 images.forEach(function(element){
     var thumb = document.createElement('img');
     thumb.setAttribute('src', element.img);
+    thumb.setAttribute('title', element.title);
+    thumb.setAttribute('discr', element.discr);
+    thumb.innerHTML = t++;
     thumb.className = 'thumb';
     allThumb.appendChild(thumb);    
 });
 
 
-var thumbElement = document.querySelectorAll('.thumblist img');
 
-function thumbClick(){
-    main.style.backgroundImage = 'url(' + thumbElement[0].src + ')';
-};
-thumbElement[0].addEventListener('click', thumbClick);
+// ('.thumblist img').click(function() {
+//     t = '.thumblist img'.index(this);
+// });
+
+
+
+// allThumb.forEach(function(element) {
+//   element.addEventListener('click', slider);
+// });
+
+
+// function whichThumbClicked(){
+//     
+//     thumbElement.forEach(function(element, index){
+//         element.addEventListener('click', thumbClick );
+//     });
+//     return element;
+// };
+
+// 
+// function handleClick(function(e) {
+//        t = e.target.getAttribute('src');
+// };
+// thumbElement.addEventListener('click', handleClick);
+// 
+// var thumbElement = document.querySelectorAll('.thumblist img');
+// 
+// var clickedThumb = onclick.target.index;
+// 
+// function thumbClick(){
+//     main.style.backgroundImage = 'url(' + thumbElement[clickedThumb].src + ')';
+//     currentText.textContent = thumbElement[clickedThumb].class + '\n' + thumbElement[clickedThumb].id;  
+// };
+// 
+// thumbElement.forEach(function(element){
+//     element.addEventListener('click', thumbClick);   
+// });
+
+// function countIndex(){
+//     for (var t = 0; t<thumbElement.length-1; t++){
+//         if (clickedOrNot){
+//             t++;
+//             thumbClick();
+//             break;
+//         } else{
+//             t++
+//         };
+//     }; return t;
+// };
+
+
+// thumbElement.getAttribute('src').addEventListener("click", thumbClick);
+// thumbElement.addEventListener('click', thumbClick);
+// thumbElement.onclick = thumbClick;
+
 
 
 function callImage(url = false){
