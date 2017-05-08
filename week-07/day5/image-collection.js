8418492fc8fd005ea18303573bbf6fdb7320b9b2
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-08 17:48:57 +0200

    delete id variable because we create it to the dom giving the index value of each thumbnails to the id as value

--- week-07/day5/image-collection.js
+++ week-07/day5/image-collection.js
@@ -3,61 +3,51 @@ var openingTitle = '!!!!!!!!! u wont see this cat ever again !!!!!!!!!!';
 var images = [
     {
         img: 'kezd.jpg',
-        id: '0',
         title: 'massza',
         discr: 'muszi',
     },
     {
         img: 'actress.jpg',
-        id: '1',
         title: 'sir',
         discr: 'temeto',
     },
     {
         img: 'cemetry.jpg',
-        id: '2',
         title: 'sicilia',
         discr: 'seta',
     },
     {
         img: 'ejha.jpg',
-        id: '3',
         title: 'ejhakutya',
         discr: 'balaton',
     },
     {
         img: 'kod.jpg',
-        id: '4',
         title: 'legmagasabb pont',
         discr: 'mo',
     },
     {
         img: 'mama.jpg',
-        id: '5',
         title: 'nagyi',
         discr: 'kedvenc',
     },
     {
         img: 'market.jpg',
-        id: '6',
         title: 'nyami',
         discr: 'zold',
     },
     {
         img: 'office.jpg',
-        id: '7',
         title: 'munka',
         discr: 'uk',
     },
     {
         img: 'rails.jpg',
-        id: '8',
         title: 'vonat',
         discr: 'tenger',
     },
     {
         img: 'slide.jpg',
-        id: '9',
         title: 'haha',
         discr: 'tip',
     }
