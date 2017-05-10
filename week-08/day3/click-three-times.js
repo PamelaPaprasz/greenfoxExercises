ad476dfe7cac560f5af21487cd7dea45b86b517f
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-10 16:21:47 +0200

    now it can handle time using a boolean variable

--- week-08/day3/click-three-times.js
+++ week-08/day3/click-three-times.js
@@ -2,15 +2,25 @@
 
 var button = document.querySelector('button');
 var text = document.querySelector('p');
-
-var click = -1;
+var click = 0;
+var time = false;
 
 var clickCounter = function(){
-    click++;   
+    click++;
+    if (click >= 3 && time === true){
+        text.textContent = '5 seconds ellapsed and clicked 3 times';
+    }   
+}
+
+
+var delayDisplay = function(){
+    
     if (click >= 3){
         text.textContent = '5 seconds ellapsed and clicked 3 times';
+    } else{
+        time = true;
     }
 }
 
 button.addEventListener('click', clickCounter);    
-setTimeout(clickCounter, 5000)
+setTimeout(delayDisplay, 5000)
