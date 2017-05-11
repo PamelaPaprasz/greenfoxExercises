fb1854ae18b5d5b15920ef4349a5b4fd1af145c5
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-11 14:28:17 +0200

    giphies work

--- week-08/day4/giphy.js
+++ week-08/day4/giphy.js
@@ -1,5 +1,7 @@
 var dolphin =  new XMLHttpRequest();
 var container = document.querySelector('div');
+var allImages = document.querySelectorAll('img');
+var gifOnOff = 0;
 
 dolphin.open('GET', 'http://api.giphy.com/v1/gifs/search?q=dolphin&api_key=dc6zaTOxFJmzC', true);
 dolphin.send();
@@ -13,12 +15,21 @@ dolphin.onreadystatechange = function(){
 
 
 var createDolphin = function(dolphinImagesData){
-    for (var j = 0; j < 16; j++){
+    dolphinImagesData.forEach (function(element){
         var image = document.createElement('img');
-        image.setAttribute('src', dolphinImagesData[j].images.original_still.url);
+        image.setAttribute('src', element.images.original_still.url);
+        image.addEventListener('click', function(){
+            gifOnOff++;
+            if (gifOnOff %2 !== 0){
+                image.setAttribute('src', element.images.original.url);    
+            } else{
+                image.setAttribute('src', element.images.original_still.url);
+            }
+        })
         container.appendChild(image);    
-    }
+    })
 }
+
 
 
    
