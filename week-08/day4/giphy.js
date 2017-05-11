5797c3b13232a2ffc097ec1c66432c3b8ae6179c
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-11 13:16:03 +0200

    feat(createDolphin):first 16 dolphin images are visible now in my file

--- week-08/day4/giphy.js
+++ week-08/day4/giphy.js
@@ -1,19 +1,24 @@
 var dolphin =  new XMLHttpRequest();
+var container = document.querySelector('div');
 
 dolphin.open('GET', 'http://api.giphy.com/v1/gifs/search?q=dolphin&api_key=dc6zaTOxFJmzC', true);
+dolphin.send();
 
 dolphin.onreadystatechange = function(){
     if (dolphin.readyState === 4 && dolphin.status === 200){
         var dolphinDatas = JSON.parse(dolphin.response);
-        // console.log(dolphinDatas);
-        for (var i = 0; i < 16; i++){
-            console.log(dolphinDatas.data[i].images.original_still.url);
-        }
+        createDolphin(dolphinDatas.data);
     }       
 }
-dolphin.send();
 
 
+var createDolphin = function(dolphinImagesData){
+    for (var j = 0; j < 16; j++){
+        var image = document.createElement('img');
+        image.setAttribute('src', dolphinImagesData[j].images.original_still.url);
+        container.appendChild(image);    
+    }
+}
 
 
    
