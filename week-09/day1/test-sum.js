c02b2bc82f0e6b1051a615d7a25bf8a2dd0db719
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-15 15:10:55 +0200

    first 4 tests are ok

--- week-09/day1/test-sum.js
+++ week-09/day1/test-sum.js
@@ -5,9 +5,14 @@ var test = require('tape');
 var sum = require('./sum.js');
 
 test('sum equal or not', function (t){
-    var actual = sum([1, 2, 3, 4, 5]);
-    var expected = 15;
+    var actual = sum([1, 2, 3, 4, 15]);
+    var expected = 25;
 
     t.equal(actual, expected);
+    t.equal(sum([0]), 0);
+    t.equal(sum([2, 4, 6]), 12);
+    t.equal(sum([5]), 5);
+    t.equal(sum([]),'');
+    t.equal(sum(['Pami']),'');
     t.end();    
 });
