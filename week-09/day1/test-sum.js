783e086985b5eabc19b57eba2adec9eded310a68
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-15 16:19:24 +0200

    tests are organized and throws used properly

--- week-09/day1/test-sum.js
+++ week-09/day1/test-sum.js
@@ -7,11 +7,26 @@ var sum = require('./sum.js');
 test('sum equal or not', function (t){
     var actual = sum([1, 2, 3, 4, 15]);
     var expected = 25;
-
     t.equal(actual, expected);
-    t.equal(sum([2, 4, 6]), 12);
+    t.end();    
+});
+
+test('empty list', function (t){
+    t.throws(function(){sum([])}, 'no element in the list');
+    t.end();
+});
+
+test('array value is null', function (t){
+    t.throws(function(){sum(null)}, 'array value is null');
+    t.end();
+});    
+    
+test('array has string or not', function (t){
+    t.throws(function(){sum([1, 'Pami', 3, 4])}, 'array contains string');    
+    t.end();
+});
+
+test('sum only one element', function (t){
     t.equal(sum([5]), 5);
-    t.throws(sum, [], 'no element in the list');
-    t.equal(sum(['Pami']),'');
     t.end();    
 });
