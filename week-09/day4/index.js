2f11f83437d8756fee1873ff2d6144efae11d3ad
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-18 14:12:11 +0200

    creating books endpoint to get all the books related data in a table

--- week-09/day4/index.js
+++ week-09/day4/index.js
@@ -19,29 +19,65 @@ var conn = mysql.createConnection({
 //   }
 //   console.log("Connection established");
 // });
-
-const country = 'USA';
-const city = 'Atlanta';
-const query =  {
-    sql: "SELECT * FROM author WHERE country = ? AND home_city = ?", 
-    values: [country, city]    
-}
+// 
+// const country = 'USA';
+// const city = 'Atlanta';
+// const query =  {
+//     sql: "SELECT * FROM author WHERE country = ? AND home_city = ?", 
+//     values: [country, city]    
+// }
+// 
+// app.get('/', function get(req, res){
+//     var result = [];
+//     console.log(result);
+//     conn.query(query, function(err,rows){
+//         if (err){
+//             console.log('PARAM', err);
+//         } else{
+//             rows.forEach(row=>{
+//                 result.push(row.aut_name);
+//             });
+//         }
+//         res.send(result);
+        // conn.query("SELECT * FROM author;",function(err,rows){
+        // console.log("Data received from Db:\n");
+        // console.log(row,aut_name);
+//     });    
+// });
 
 app.get('/', function get(req, res){
-    var result = [];
-    console.log(result);
-    conn.query(query, function(err,rows){
+    var htmlList = '<ul>';
+    var endHtml = '</ul>';
+    
+    conn.query("SELECT book_name FROM book_mast;", function(err,rows){
         if (err){
             console.log('PARAM', err);
         } else{
             rows.forEach(row=>{
-                result.push(row.aut_name);
+                htmlList += '<li>' + row.book_name + '</li>';
             });
-        }
-        res.send(result);
-        // conn.query("SELECT * FROM author;",function(err,rows){
-        // console.log("Data received from Db:\n");
-        // console.log(row,aut_name);
+        } htmlList += endHtml;
+        res.send(htmlList);
+    });    
+});
+
+app.get('/books', function get(req, res){
+    var tableStart = '<table>';
+    var tableEnd = '</table>';
+    
+    conn.query("SELECT book_name, author.aut_name, category.cate_descrip, publisher.pub_name, book_price FROM book_mast LEFT JOIN author ON book_mast.aut_id = author.aut_id LEFT JOIN category ON book_mast.cate_id = category.cate_id LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id;", function(err,rows){
+        if (err){
+            console.log('PARAM', err);
+        } else{
+            rows.forEach(row=>{
+                tableStart += '<tr> <td>' + row.book_name + '</td>';
+                tableStart += '<td>' + row.aut_name + '</td>';
+                tableStart += '<td>' + row.cate_descrip + '</td>';
+                tableStart += '<td>' + row.pub_name + '</td>';
+                tableStart += '<td>' + row.book_price + '</td> </tr>' ;
+            });
+        } tableStart += tableEnd;
+        res.send(tableStart);
     });    
 });
 
