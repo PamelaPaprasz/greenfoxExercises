957cf87d25f70bbc616a9897c61487fbef065174
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-24 18:44:15 +0200

    server is working has 3 get endpoints

--- week-10/musicplayer/foxplayer.js
+++ week-10/musicplayer/foxplayer.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const app = express();
+app.use('/assets', express.static('assets'));
+app.use('/img', express.static('img'));
 
 var playlists = [
 	{ "id": 1, "title": "Favorites", "system": 1},
@@ -10,12 +12,22 @@ var playlists = [
 	{ "id": 5, "title": "Fox house", "system": 0},
 ];
 
+var tracks = [
+	{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
+	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" },{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "/assets/Purple_Drift.mp3" },{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" },{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" },{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" },{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" },{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }
+];
+
 app.get('/', function(req, res){
-    res.send('hello word jbovjbojvbo');
+    res.sendFile(__dirname + '/foxplayer.html');
 });
+
 
 app.get('/playlists', function(req, res){
     res.send(playlists);
+});
+
+app.get('/playlists-tracks/', function(req, res){
+    res.send(tracks);
 });
 
 app.listen(3000, function(){
