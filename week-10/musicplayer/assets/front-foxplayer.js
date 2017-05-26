ad6ca37644fb141e2ba342132061bffed191c598
Pamela Paprasz <papraszpamela@gmail.com>
2017-05-26 15:57:44 +0200

    tracks are played one after another and highlight is working too

--- week-10/musicplayer/assets/front-foxplayer.js
+++ week-10/musicplayer/assets/front-foxplayer.js
@@ -5,9 +5,34 @@ var songContainer = document.querySelector('.actual-track-list');
 var sourceSong = document.querySelector('audio');
 var polipX = document.querySelector('.ever-list');
 var blackPlus = document.querySelector('.black-plus');
+var allTracksPlaylist = document.querySelector('.all-tracks');
+var favoriteTracksPlaylist = document.querySelector('.favorite-tracks');
 var rankNumber;
 var method;
+var serverTracklist;
+var currentTrack;
 
+
+function postNewPlaylistToServer(userInput, callback){
+	var xhr = new XMLHttpRequest();
+	method = "POST";
+	
+	xhr.open(method, 'http://localhost:3000/playlists', true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.setRequestHeader('Accept', 'application/json');
+	
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200){
+			var requestedData = JSON.parse(xhr.response);
+			callback(requestedData);
+		}
+	}
+	var data = {
+		title: userInput,
+		system: 0
+	}
+	xhr.send(JSON.stringify(data));
+}
 
 
 function ajax(url, method, callback) {
@@ -48,14 +73,52 @@ function createPlaylist(serverData) {
 		xSign.addEventListener('click', function(){
 			ajax('http://localhost:3000/playlists/' + xSign.id, 'DELETE', createPlaylist);
 		})
-		ajax('http://localhost:3000/playlists-tracks/', 'GET', createTracklist);	
+		ajax('http://localhost:3000/playlists-tracks/', 'GET', createTracklist);
+	
+		
+		trackBox.addEventListener('click', function(){
+			
+			allTracksPlaylist.classList.remove('active-track');
+			favoriteTracksPlaylist.classList.remove('active-track');
+			
+			var trackBoxList = document.querySelectorAll('.track-box');
+			
+			trackBoxList.forEach(function(element){
+				element.classList.remove('active-track')
+			})	
+			trackBox.classList.add('active-track');
+		})	
     });
 }
 
 
+allTracksPlaylist.addEventListener('click', function(){
+	var trackBoxList = document.querySelectorAll('.track-box');
+	
+	trackBoxList.forEach(function(element){
+		element.classList.remove('active-track')
+	})	
+	favoriteTracksPlaylist.classList.remove('active-track');
+	
+	allTracksPlaylist.classList.add('active-track');
+})
+
+
+favoriteTracksPlaylist.addEventListener('click', function(){
+	var trackBoxList = document.querySelectorAll('.track-box');
+	
+	trackBoxList.forEach(function(element){
+		element.classList.remove('active-track')
+	})	
+	allTracksPlaylist.classList.remove('active-track');
+	
+	favoriteTracksPlaylist.classList.add('active-track');
+})
+
 
 function createTracklist(serverData) {
     rankNumber = 0
+	serverTracklist = serverData;
 	songContainer.innerHTML = '';
     serverData.forEach(function(element, index){
         rankNumber++
@@ -74,32 +137,34 @@ function createTracklist(serverData) {
         songBox.appendChild(duration);
 		
         songBox.addEventListener('click', function(){
-            playClickedTrack(element.path)
+			currentTrack = index;
+			playTrack();
         })
     });
 }
 
 
-function postNewPlaylistToServer(userInput, callback){
-	var xhr = new XMLHttpRequest();
-	method = "POST";
-
-	xhr.open(method, 'http://localhost:3000/playlists', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Accept', 'application/json');
-
-	xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200){
-			var requestedData = JSON.parse(xhr.response);
-            callback(requestedData);
-		}
-	}
-    var data = {
-        title: userInput,
-        system: 0
-    }
-	xhr.send(JSON.stringify(data));
+function playTrack(){
+	sourceSong.setAttribute('src', serverTracklist[currentTrack].path)
+	sourceSong.play();
+	highlight();
 }
+
+function highlight(){
+	var songBoxList = document.querySelectorAll('.song-box');
+	songBoxList.forEach(function (element){
+		element.classList.remove('active')
+	})
+	songBoxList[currentTrack].classList.add('active');
+	
+}
+
+
+sourceSong.addEventListener('ended', function(){
+	currentTrack++;
+	playTrack()
+});
+
 
 
 function addNewPlaylist(){
@@ -108,10 +173,6 @@ function addNewPlaylist(){
 }
 blackPlus.addEventListener('click', addNewPlaylist)
 
-function playClickedTrack(path){
-    sourceSong.setAttribute('src', path)
-    sourceSong.play();
-}
 
 ajax('http://localhost:3000/playlists', 'GET', createPlaylist);
 
