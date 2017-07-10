c4137500a042380a0b94659b1e43911a3db26477
Pamela Paprasz <papraszpamela@gmail.com>
2017-07-10 09:47:37 +0200

    clean

--- final_exam/1.js
+++ final_exam/1.js
@@ -1,17 +1,7 @@
 // Create a function that takes a matrix (list of lists) of numbers and sums all the numbers.
 
 function summMatrix(matrix) {
-  // var list1 = matrix[0];
-  // var list2 = matrix[1];
   var summAll = 0;
-
-  // for (var i = 0; i < list1.length; i++){
-  //   summAlll += list1[i]
-  // }
-
-  // for (var j = 0; j < list2.length; j++){
-  //   summAlll += list2[j]
-  // }
 
   for (var i = 0; i < matrix.length; i++){
     for (var j = 0; j < matrix[i].length; j++){
