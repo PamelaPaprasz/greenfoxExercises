511027aaf109766c64df530cab699369d8378269
Pamela Paprasz <papraszpamela@gmail.com>
2017-07-10 09:45:40 +0200

    now it is dynamic

--- final_exam/1.js
+++ final_exam/1.js
@@ -3,20 +3,22 @@
 function summMatrix(matrix) {
   var list1 = matrix[0];
   var list2 = matrix[1];
-  var summAll = [];
-  var summAlll = 0;
+  var summAll = 0;
+  var arr = []
 
-  for (var i = 0; i < list1.length; i++){
-    summAll.push(list1[i])
-    summAlll += list1[i]
+  // for (var i = 0; i < list1.length; i++){
+  //   summAlll += list1[i]
+  // }
+
+  // for (var j = 0; j < list2.length; j++){
+  //   summAlll += list2[j]
+  // }
+
+  for (var i = 0; i < matrix.length; i++){
+    for (var j = 0; j < matrix[i].length; j++){
+      summAll += matrix[i][j]
+    }
   }
-
-  for (var j = 0; j < list2.length; j++){
-    summAll.push(list2[j])
-    summAlll += list2[j]
-  }
-  return summAlll;
-
+  return summAll;
 }
-
 console.log(summMatrix([[1, 2, 3], [5, 6]]))
